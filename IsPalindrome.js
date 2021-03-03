90c2eae442e27e8e4fe4e93f600361b07c548d61
Jawad Boudarat <jawadboudarat@gmail.com>
2021-03-03 19:12:40 +0000

    Palindromo sin acentos

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -1,7 +1,8 @@
-const text = "la casa de Emilio"
+const text = "A mamá Roma le ,aviva el amor ,a papá y a papá Roma le !aviva el amor a mamá"
 
 function isPalindrome (str) {
-    str = str.replace(/\.|\s|\,/g,"")
+    str = str.replace(/\.|\s|\!|\,/g,"")
+    str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
     const lowered = str.toLowerCase()
     const splitted = lowered.split('')
     const reversed = splitted.reverse()
