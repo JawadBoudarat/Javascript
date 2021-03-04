5569a384cc77df23859790970a738256c5940569
Jawad Boudarat <jawadboudarat@gmail.com>
2021-03-04 13:21:05 +0000

    Prettier format

--- IsPalindrome.js
+++ IsPalindrome.js
@@ -1,16 +1,16 @@
-const text = "A mamá Roma le ,aviva el amor ,a papá y a papá Roma le !aviva el amor a mamá"
+const text =
+  "A mamá Roma le ,aviva el amor ,a papá y a papá Roma le !aviva el amor a mamá";
 
-function isPalindrome (str) {
-    str = str.replace(/\.|\s|\!|\,/g,"")
-    str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-    const lowered = str.toLowerCase()
-    const splitted = lowered.split('')
-    const reversed = splitted.reverse()
-    const joined = reversed.join("")
+function isPalindrome(str) {
+  str = str.replace(/\.|\s|\!|\,/g, "");
+  str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const lowered = str.toLowerCase();
+  const splitted = lowered.split("");
+  const reversed = splitted.reverse();
+  const joined = reversed.join("");
 
-    return lowered == joined
-
+  return lowered == joined;
 }
 
-const a = isPalindrome(text)
-console.log(a)
+const a = isPalindrome(text);
+console.log(a);
--- Repeated.js
+++ Repeated.js
@@ -1,25 +1,23 @@
-const text = "En el barco rojo del puerto, hay muchos barco barco coches en doble fila que no se han percatado de que no van a montar en el barco!"
+const text =
+  "En el barco rojo del puerto, hay muchos barco barco coches en doble fila que no se han percatado de que no van a montar en el barco!";
 
-function mostRepeated(str){
-
-    str = str.toLowerCase()
-    str = str.replace(/\.|\,|\!/g,"")
-    const splitted = str.split(" ")
-    const reduced = splitted.reduce(function(acc,el){
-        if (acc[el]){
-            acc[el]++
-        }else{
-        acc[el]=1
-        }
-        return acc
-    },{})
-    return Object.entries(reduced).reduce(function(acc,el){
-        if (acc[1]>el[1])
-        return acc
-        else
-        return el
-    })
+function mostRepeated(str) {
+  str = str.toLowerCase();
+  str = str.replace(/\.|\,|\!/g, "");
+  const splitted = str.split(" ");
+  const reduced = splitted.reduce(function (acc, el) {
+    if (acc[el]) {
+      acc[el]++;
+    } else {
+      acc[el] = 1;
     }
+    return acc;
+  }, {});
+  return Object.entries(reduced).reduce(function (acc, el) {
+    if (acc[1] > el[1]) return acc;
+    else return el;
+  });
+}
 
-const a = mostRepeated(text)
-console.log(a)
+const a = mostRepeated(text);
+console.log(a);
