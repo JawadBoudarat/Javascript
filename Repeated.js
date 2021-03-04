60ecc34ee42a6f7011c57b6df2f938169887999a
Jawad Boudarat <jawadboudarat@gmail.com>
2021-03-04 12:54:50 +0000

    Palabra más repetida

---
+++ Repeated.js
@@ -0,0 +1,25 @@
+const text = "En el barco rojo del puerto, hay muchos barco barco coches en doble fila que no se han percatado de que no van a montar en el barco!"
+
+function mostRepeated(str){
+
+    str = str.toLowerCase()
+    str = str.replace(/\.|\,|\!/g,"")
+    const splitted = str.split(" ")
+    const reduced = splitted.reduce(function(acc,el){
+        if (acc[el]){
+            acc[el]++
+        }else{
+        acc[el]=1
+        }
+        return acc
+    },{})
+    return Object.entries(reduced).reduce(function(acc,el){
+        if (acc[1]>el[1])
+        return acc
+        else
+        return el
+    })
+    }
+
+const a = mostRepeated(text)
+console.log(a)
